0d53c89d4ab54ae02f6acc9cbde0172cb556ece5
AC <acsimoes446@gmail.com>
2024-09-22 13:05:46 -0300

    feat: add estrutura projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+# tarefa_mobile
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/tarefa_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tarefa_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
